1d86015703c3a1701e73bfd05026805c2cfe51df
Segun Oladiran <sheghunoladiran9@gmail.com>
2025-11-25 20:33:57 +0100

    decimal integers done

--- src/002_variables_and_data/number_system.cpp
+++ src/002_variables_and_data/number_system.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
-
 int main() {
-  int number1 = 15; // Decimal  
-  int number2 = 017; // Octal  
-  int number3 = 0x0F; // Hexadecimal  
-  int number4 = 0b1111; // Binary  
-  
+  int number1 = 15;     // Decimal
+  int number2 = 017;    // Octal
+  int number3 = 0x0F;   // Hexadecimal
+  int number4 = 0b1111; // Binary
+
   std::cout << "Decimal: " << number1 << std::endl;
   std::cout << "Octal: " << number2 << std::endl;
   std::cout << "Hexadecimal: " << number3 << std::endl;
