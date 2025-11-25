cb5b2cdf9bbdb00bfc41dd319d12e36fce1b98cd
Segun Oladiran <sheghunoladiran9@gmail.com>
2025-11-25 08:22:09 +0100

    take inputs from terminal

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
 
-int addNumbers(int a, int b) {
-  return a + b;
-}
+int addNumbers(int a, int b) { return a + b; }
 
-int multiplyNumbers(int a, int b) {
-  return a * b;
-}
+int multiplyNumbers(int a, int b) { return a * b; }
 
 int main() {
   int first_number = {3};
   int second_number = {5};
-  auto lang = "C++";
-  std::cout << "Hello and welcome to " << lang << "!\n";
-
-  for (int i = 1; i <= 5; i++) {
-    std::cout << "i = " << i << std::endl;
-  }
 
   std::cout << "First number: " << first_number << "\n";
   std::cout << "Second number: " << second_number << "\n";
@@ -26,6 +16,17 @@ int main() {
 
   int product = multiplyNumbers(first_number, second_number);
   std::cout << "Product: " << product << "\n";
-  
+
+  int age = -1;
+  std::string name = "";
+
+  std::cout << "Please enter your name: " << "\n";
+  std::cin >> name;
+
+  std::cout << "Please enter your age: " << "\n";
+  std::cin >> age;
+
+  std::cout << "Hello, " << name << "! You are " << age << " years old.\n";
+
   return 0;
 }
