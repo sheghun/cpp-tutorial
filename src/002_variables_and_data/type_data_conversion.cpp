dfde4f6b451675fc9e9f07e9398b35772a057496
Segun Oladiran <sheghunoladiran9@gmail.com>
2025-11-28 23:40:13 +0100

    type data conversion done

--- src/002_variables_and_data/type_data_conversion.cpp
+++ src/002_variables_and_data/type_data_conversion.cpp
@@ -17,4 +17,24 @@ int main() {
     std::cout << "the value of x is: " << x << std::endl;
     std::cout << "sizeof x: " << sizeof(x) << std::endl;
 
+    // implicit conversion
+    double a{12.5};
+    double b{34.6};
+
+    int sum = a + b;
+    std::cout << "implicit sum: " << sum << std::endl;
+
+    // explicitly cast and sum
+    sum = static_cast<int>(a) + static_cast<int>(b);
+    std::cout << "explicit cast before sum: " << sum << std::endl;
+
+    // explicitly cast sum up
+    sum = static_cast<int>(a + b);
+    std::cout << "explicit sum: " << sum << std::endl;
+
+    // old style c-cast
+    double pi{3.14};
+    int init_pi = (int) pi;
+    std::cout << "pi: " << pi << std::endl;
+    std::cout << "init_pi: " << init_pi << std::endl;
 }
