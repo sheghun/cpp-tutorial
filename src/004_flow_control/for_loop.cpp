792f626bbe949d90c22032051a7ae733a076497d
Segun Oladiran <sheghunoladiran9@gmail.com>
2025-12-02 22:32:09 +0100

    for loop assignment done

--- src/004_flow_control/for_loop.cpp
+++ src/004_flow_control/for_loop.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
+
+void print_multiples_of_3() {
+
+
+    std::vector<int> numbers{1, 21, 4, 5, 7, 3, 9, 3, 5, 17, 15};
+
+    // Don't modify anything above this line
+    // Your code should go below this line
+
+    for (auto value: numbers) {
+        if (value % 3 == 0) {
+            std::cout << value << " ";
+        }
+    }
+
+
+    // Your code should go above this line
+    // Don't modify anything below this line
+}
+
 int main() {
     constexpr int count{10};
     for (size_t i = 0; i < count; i++) {
@@ -21,4 +41,24 @@ int main() {
     for (unsigned char i{65}; i <= 90; ++i) {
         std::cout << i << ((i < 90) ? " " : "");
     }
+    std::cout << std::endl;
+
+
+    std::cout << "----------ranged loop-----------" << std::endl;
+
+    // range loop
+    int bag_of_values[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for (int value: bag_of_values) {
+        std::cout << "value: " << value << std::endl;
+    }
+
+    for (size_t i{0}; i < 10; ++i) {
+        std::cout << "value: " << bag_of_values[i] << std::endl;
+    }
+
+    print_multiples_of_3();
+
+    return 0;
 }
+
